6bdf11a0f240df7e78af17bba152a379e8d1b292
FM-Akib <muntasirakib08@gmail.com>
2024-01-14 12:13:52 +0600

    Api Loaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+<LoadApi></LoadApi>
+
+
+
+
     </div>
   );
+}
+
+function LoadApi() {
+
+  const [users,setUser]=useState([]);
+  useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUser(data))
+  },[])
+
+  return (
+   <div>
+    {
+      users.map(user =><User name={user.name}></User>)
+    }
+     
+   </div>
+  );
+}
+
+function User(props){
+  return (
+    <div>
+      <h1>Name:{props.name}</h1>
+    </div>
+  )
 }
 
 export default App;
